cddf4b6789c357e07d025a69fc47db76d9007cb5
Gayathriamirtharaj <gayathriamirtharaj@gmail.com>
2020-07-03 17:42:42 +0530

    intial commit

---
+++ index1.ts
@@ -0,0 +1,13 @@
+class cricket{
+  team1players:number[];
+  team2players:number[]; 
+  timer:number;
+   
+  constructor(){
+      this.team1players=[];
+      this.team2players=[];
+  };
+  
+  
+
+}
